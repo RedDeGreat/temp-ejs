bcf979c18fa1399d167ec182d8b32343d79c3d47
Katacoda Scenario <scenario@katacoda.com>
2020-12-12 05:23:22 +0000

    Hello world!

--- app.js
+++ app.js
@@ -5,6 +5,13 @@ const ejs = require('ejs');
 const port = 3000;
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+app.use(express.static('public'))
+
+app.get('/',function(req,res){
+    res.send("Hello world!");
+});
 
 
 app.listen(port,()=>console.log('Listening....' + port));
